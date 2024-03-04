248509f85cc45be85d9c148840a11aa82ef9ee1c
ПК <saponembigas@gmail.com>
2024-03-04 17:34:53 +0700

    Создание эхо-бота телеграм

--- tg-bot/src/main/java/com/education/aitgbot/tgbot/TgBotApplication.java
+++ tg-bot/src/main/java/com/education/aitgbot/tgbot/TgBotApplication.java
@@ -2,8 +2,9 @@ package com.education.aitgbot.tgbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TgBotApplication {
 
 	public static void main(String[] args) {
